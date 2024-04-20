d1fa4f1e904fd6f101e453da32de6b7e77453ef5
YqyYun <yqyengland111@163.com>
2024-04-20 21:11:39 +0800

    MergeIntervals.c improvement

--- leetcode/leetbook/array_and_string/MergeIntervals.c
+++ leetcode/leetbook/array_and_string/MergeIntervals.c
@@ -33,9 +33,11 @@ int** merge(int** intervals, int intervalsSize, int* intervalsColSize, int* retu
 
     //flag pointer 在堆中分配，若不考虑栈溢出可以考虑在栈内分配。
     int* flags = (int*)calloc(row, row * sizeof(int));
-    //flag pointer 在栈内分配
-    //int flags[row] = {0};
-
+    //flag pointer 在栈内分配, 在栈内分配性能有提升，609 ms 击败 5.05% 使用 C 的用户 
+    //int flags[row];
+    //for (int i = 0; i < row; ++i) {
+        //flags[i] = 0;
+    //}
     int** returnIntervals = (int**)malloc(row * sizeof(int*));
 
     for(int i = 0; i < row; ++i) {
@@ -146,6 +148,103 @@ int** merge(int** intervals, int intervalsSize, int* intervalsColSize, int* retu
     
 }
 
+/**
+* 方法一改进：直接使用递归方式合并还未被合并的区间元素，删掉与returnIntervals合并去重的循环。性能提升一倍
+* 时间复杂度和空间复杂度同上。
+* 执行用时分布 347 ms 击败 8.50% 使用 C 的用户 消耗内存分布 12.22 MB 击败 59.82% 使用 C 的用户
+*
+**/
+int** merge_0(int** intervals, int intervalsSize, int* intervalsColSize, int* returnSize, 
+    int** returnColumnSizes) {
+    //returnSize = (int*)malloc(sizeof(int));
+    int row = intervalsSize;
+    //intervals[i].length == 2;
+    int col = intervalsColSize[0];
+
+    int ret_row = 0;
+
+    //flag pointer 在堆中分配，若不考虑栈溢出可以考虑在栈内分配。
+    int* flags = (int*)calloc(row, row * sizeof(int));
+    //flag pointer 在栈内分配
+    //int flags[row];
+    //for (int i = 0; i < row; ++i) {
+     //   flags[i] = 0;
+    //}
+
+    int** returnIntervals = (int**)malloc(row * sizeof(int*));
+
+    for(int i = 0; i < row; ++i) {
+        // 过滤已经合并的区间元素
+        if( flags[i] ) {
+            continue;
+        }
+        flags[i] = 1;
+
+        int left_1, left_2, right_1, right_2, merge_1, merge_2;
+
+         merge_1 = intervals[i][0];
+         merge_2 = intervals[i][1];
+
+
+        for(int j = i + 1; j < row; ++j) {
+            if( flags[j] ) {
+                continue;
+            }
+            if( merge_1 < intervals[j][0] ) {
+                left_1 = merge_1;
+                left_2 = merge_2;
+                right_1 = intervals[j][0];
+                right_2 = intervals[j][1];
+            }
+            else {
+                left_1 = intervals[j][0];
+                left_2 = intervals[j][1];
+                right_1 = merge_1;
+                right_2 = merge_2;
+            }
+            // 左区间右端点可能处在3个位置之一，右区间左边，右区间内，右区间右边
+            if( left_2 < right_1 ) {
+                ;
+            }
+            else if( left_2 < right_2 ) {
+                merge_1 = left_1;
+                merge_2 = right_2;
+
+                flags[j] = 1;
+            }
+            else {
+                merge_1 = left_1;
+                merge_2 = left_2;
+
+                flags[j] = 1;
+            }
+
+        }
+
+        returnIntervals[ret_row] = (int*)malloc(col * sizeof(int));
+        returnIntervals[ret_row][0] = merge_1;
+        returnIntervals[ret_row][1] = merge_2;
+        ++ret_row;
+    }
+
+    // 模版代码
+    *returnSize = ret_row;
+
+    *returnColumnSizes = (int*)malloc((*returnSize) * sizeof(int));
+
+    for(int i = 0; i < *returnSize; ++i) {
+        (*returnColumnSizes)[i] = col;
+    }
+    // qsort(returnIntervals, ret_row, sizeof(int*), compare);
+
+    // returnIntervals中还存在可以互相合并的区间，因此递归调用直到不存在可以合并的区间为止。
+    if (row == ret_row) {
+        return returnIntervals;
+    }
+    return merge(returnIntervals, ret_row, intervalsColSize, returnSize, returnColumnSizes);
+    // return returnIntervals;
+    
+}
 int main(void) {
     return 0;
 }
